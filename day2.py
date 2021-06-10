6d5841a0f492e5d80f5c89da245790ad88a9a4eb
suraj singh <85230317+suraj-singh-au18@users.noreply.github.com>
2021-06-10 20:42:35 +0530

    update

---
+++ day2.py
@@ -0,0 +1,24 @@
+[33mcommit b25d51b12faa9d47f70949118f4b1d0b4f8146f2[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: suraj singh <suraj.singh7318006789@gmail.com>
+Date:   Thu Jun 10 20:36:57 2021 +0530
+
+    a=1
+    b=3
+    c=2
+    if (a>=b) and (b >= c):
+    largest = a
+    elif (b>= a) and (b>=c):
+    largerst =b
+    else:
+    largest = c
+    print("the largest number is",largest)
+
+[33mcommit a5d64a884b7af82237945d874f8d02432fce1cc6[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: suraj singh <suraj.singh7318006789@gmail.com>
+Date:   Thu Jun 10 15:39:10 2021 +0530
+
+    number1 = input("enter first number:" )
+    number2 = input("\nenter second number:" )
+    
+    sum = int(number1)+int(number2)
+    print('the sum of {0} and {1}) is {2}'.format(number1,number2,sum))
